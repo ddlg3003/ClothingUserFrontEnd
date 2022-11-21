e21eea2eab7a7732f0c118b038ea959df859af69
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2022-11-21 13:40:22 +0700

    change checkout 1 chut

--- src/features/checkout.js
+++ src/features/checkout.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    isCheckout: false,
+    isCheckout: sessionStorage.getItem('cartItems') ? true : false,
 };
 
 export const checkout = createSlice({
