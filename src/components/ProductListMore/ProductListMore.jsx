12e90f20b334c2b219038fbf25127aba26c9f18a
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2022-11-24 02:20:05 +0700

    clean some notnecessary state hooks for cleaner code

--- src/components/ProductListMore/ProductListMore.jsx
+++ src/components/ProductListMore/ProductListMore.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Products from '../Products/Products';
 import { Box, CircularProgress, Pagination, Stack } from '@mui/material';
 import { useGetProductsQuery } from '../../services/clothing';
@@ -8,33 +8,31 @@ import useStyles from './styles';
 
 const ProductListMore = () => {
     const classes = useStyles();
+    // Hook for set query string state
     const [searchParams, setSearchParams] = useSearchParams();
 
     // Query string validation, isInteger check if the passing arg is an integer or not 
     // 3 main query: page, limit, cat
-    const pageNum = parseInt(searchParams.get(PRODUCT_QUERY_STRING[0])); // Get the query of page for validation
-    const pageInit = Number.isInteger(pageNum) && pageNum > 0 ? pageNum : 1;
-
     const limitNum = parseInt(searchParams.get(PRODUCT_QUERY_STRING[1])); // Get the query of limit for validation
     const limitInit = Number.isInteger(limitNum) && limitNum > 0 ? limitNum : LIMIT;
 
     const catNum = parseInt(searchParams.get(PRODUCT_QUERY_STRING[2])); // Get the query of cat for validation
     const catInit = Number.isInteger(catNum) ? catNum : '';
 
-    const [page, setPage] = useState(pageInit);
+    const pageNum = parseInt(searchParams.get(PRODUCT_QUERY_STRING[0])); // Get the query of page for validation
+    const pageInit = Number.isInteger(pageNum) && pageNum > 0 ? pageNum : 1;
+
     const { data, isFetching } = useGetProductsQuery({
-        pageNumber: page,
+        pageNumber: pageInit,
         pageSize: limitInit,
         cat: catInit,
     });
 
     const onPageChange = (e, value) => {
-        console.log(value);
-        setPage(value);
-
         let query = { [PRODUCT_QUERY_STRING[0]]: value, [PRODUCT_QUERY_STRING[1]]: limitInit };
 
-        if(searchParams.get('cat')) {
+        // Check if cat query exist 
+        if(searchParams.get(PRODUCT_QUERY_STRING[2])) {
             query = { ...query, [PRODUCT_QUERY_STRING[2]]: catInit };
         }
 
@@ -51,13 +49,13 @@ const ProductListMore = () => {
                 <>
                     <Products data={data} />
                     <Stack spacing={2} className={classes.pagination}>
-                    <Pagination
-                        count={Math.ceil(data?.numberItem / limitInit)}
-                        shape="rounded"
-                        page={page}
-                        size="large"
-                        onChange={onPageChange}
-                    />
+                        <Pagination
+                            count={Math.ceil(data?.numberItem / limitInit)}
+                            shape="rounded"
+                            page={pageInit}
+                            size="large"
+                            onChange={onPageChange}
+                        />
                     </Stack>
                 </>
             )}
