4be48fbf7a3d1043a143fc76e0925040729c5ec5
Phi Anh <phianh5@gmail.com>
2022-12-04 16:48:52 +0700

    order show comment status

--- src/components/Profile/Orders.jsx
+++ src/components/Profile/Orders.jsx
@@ -20,7 +20,6 @@ import RatingDialog from "./RatingDialog";
 const Orders = (props) => {
   const [openRatingDialog, setOpenRatingDialog] = useState(false);
 
-
   useEffect(() => {
     if (props.navSelection === "orders") {
       props.setTabValue("1");
@@ -55,7 +54,13 @@ const Orders = (props) => {
             </Box>
 
             <TabPanel value="1">
-              <Container sx={{ mt: 0, height: "460px", overflow: props.orders?.length > 0 ? "scroll" : "none" }}>
+              <Container
+                sx={{
+                  mt: 0,
+                  height: "460px",
+                  overflow: props.orders?.length > 0 ? "scroll" : "none",
+                }}
+              >
                 {props.orders?.map((order, i) => (
                   <div key={i} style={{ marginTop: i > 0 ? "40px" : "0px" }}>
                     {order.transactionMapper.map((product, proIndex) => (
@@ -117,18 +122,41 @@ const Orders = (props) => {
                           </Grid>
 
                           <Grid item>
-                            <Box>
-                              {/* <Button
-                              disabled
-                              color="black"
-                              variant="contained"
-                              component="label"
-                              style={{ color: "black" }}
-                              startIcon={<LocalShippingIcon />}
-                            >
-                              Đã giao
-                            </Button> */}
-                            </Box>{" "}
+                            {order?.ordStatus === ORDER_STATUS[2].status ? (
+                              product?.commented ? (
+                                <Button
+                                  color="black"
+                                  variant="contained"
+                                  component="label"
+                                  style={{ color: "white" }}
+                                  startIcon={<StarsIcon />}
+                                  disabled
+                                >
+                                  Đã đánh giá
+                                </Button>
+                              ) : (
+                                <Box>
+                                  <Button
+                                    color="black"
+                                    variant="contained"
+                                    component="label"
+                                    style={{ color: "white" }}
+                                    startIcon={<StarsIcon />}
+                                    onClick={() => handleClickRate(product?.id)}
+                                  >
+                                    Đánh giá
+                                  </Button>
+                                  <RatingDialog
+                                    classes={props.classes}
+                                    open={openRatingDialog === product?.id}
+                                    onClose={handleCloseRatingDialog}
+                                    orderDetails={product}
+                                  />
+                                </Box>
+                              )
+                            ) : (
+                              <></>
+                            )}
                           </Grid>
                         </Grid>
                         <Divider />
@@ -460,88 +488,105 @@ const Orders = (props) => {
                   )
                   .map((filteredOrder, i) => (
                     <div key={i} style={{ marginTop: i > 0 ? "40px" : "0px" }}>
-                      {filteredOrder.transactionMapper.map((product, proIndex) => (
-                        <Box key={`pro${proIndex}_done`}>
-                          <Grid container spacing={2} sx={{ mb: 0, mt: 1 }}>
-                            <Grid
-                              item
-                              xs
-                              container
-                              direction="column"
-                              spacing={2}
-                            >
-                              <Link
-                                to={`/products/${product.productName
-                                  .replace(URL_REGEX, "-")
-                                  .toLowerCase()}-i.${product.productId}`}
-                                className={props.classes.favoriteItems}
+                      {filteredOrder.transactionMapper.map(
+                        (product, proIndex) => (
+                          <Box key={`pro${proIndex}_done`}>
+                            <Grid container spacing={2} sx={{ mb: 0, mt: 1 }}>
+                              <Grid
+                                item
+                                xs
+                                container
+                                direction="column"
+                                spacing={2}
                               >
-                                <Grid container spacing={2}>
-                                  <Grid
-                                    item
-                                    sx={{ paddingLeft: "32px!important" }}
-                                  >
-                                    <img
-                                      src={product?.productImage}
-                                      width={80}
-                                      alt=""
-                                    />
-                                  </Grid>
-                                  <Grid item xs>
-                                    <Typography
-                                      fontSize={18}
-                                      gutterBottom
-                                      component="div"
-                                      sx={{ color: "black!important" }}
-                                    >
-                                      {product?.productName}
-                                    </Typography>
-                                    <Typography color="text.secondary">
-                                      Phân loại hàng: {product?.color} -{" "}
-                                      {product?.size}
-                                    </Typography>
-                                    <Typography color="text.secondary">
-                                      x{product?.tranQuantity}
-                                    </Typography>
-                                    <Typography
-                                      color="error"
-                                      fontWeight="bold"
-                                      fontSize={20}
-                                    >
-                                      {Intl.NumberFormat("vi-VN", {
-                                        style: "currency",
-                                        currency: "VND",
-                                      }).format(product?.tranUnitPrice)}
-                                    </Typography>
-                                  </Grid>
-                                </Grid>
-                              </Link>
-                            </Grid>
-
-                            <Grid item>
-                              <Box>
-                                <Button
-                                  color="black"
-                                  variant="contained"
-                                  component="label"
-                                  style={{ color: "white" }}
-                                  startIcon={<StarsIcon />}
-                                  onClick={() => handleClickRate(product.id)}
+                                <Link
+                                  to={`/products/${product.productName
+                                    .replace(URL_REGEX, "-")
+                                    .toLowerCase()}-i.${product.productId}`}
+                                  className={props.classes.favoriteItems}
                                 >
-                                  Đánh giá
-                                </Button>
-                                <RatingDialog
-                                  classes={props.classes}
-                                  open={openRatingDialog === product.id}
-                                  onClose={handleCloseRatingDialog}
-                                  orderDetails={product}
-                                />
-                              </Box>{" "}
+                                  <Grid container spacing={2}>
+                                    <Grid
+                                      item
+                                      sx={{ paddingLeft: "32px!important" }}
+                                    >
+                                      <img
+                                        src={product?.productImage}
+                                        width={80}
+                                        alt=""
+                                      />
+                                    </Grid>
+                                    <Grid item xs>
+                                      <Typography
+                                        fontSize={18}
+                                        gutterBottom
+                                        component="div"
+                                        sx={{ color: "black!important" }}
+                                      >
+                                        {product?.productName}
+                                      </Typography>
+                                      <Typography color="text.secondary">
+                                        Phân loại hàng: {product?.color} -{" "}
+                                        {product?.size}
+                                      </Typography>
+                                      <Typography color="text.secondary">
+                                        x{product?.tranQuantity}
+                                      </Typography>
+                                      <Typography
+                                        color="error"
+                                        fontWeight="bold"
+                                        fontSize={20}
+                                      >
+                                        {Intl.NumberFormat("vi-VN", {
+                                          style: "currency",
+                                          currency: "VND",
+                                        }).format(product?.tranUnitPrice)}
+                                      </Typography>
+                                    </Grid>
+                                  </Grid>
+                                </Link>
+                              </Grid>
+
+                              <Grid item>
+                                {product?.commented ? (
+                                  <Button
+                                    color="black"
+                                    variant="contained"
+                                    component="label"
+                                    style={{ color: "white" }}
+                                    startIcon={<StarsIcon />}
+                                    disabled
+                                  >
+                                    Đã đánh giá
+                                  </Button>
+                                ) : (
+                                  <Box>
+                                    <Button
+                                      color="black"
+                                      variant="contained"
+                                      component="label"
+                                      style={{ color: "white" }}
+                                      startIcon={<StarsIcon />}
+                                      onClick={() =>
+                                        handleClickRate(product?.id)
+                                      }
+                                    >
+                                      Đánh giá
+                                    </Button>
+                                    <RatingDialog
+                                      classes={props.classes}
+                                      open={openRatingDialog === product?.id}
+                                      onClose={handleCloseRatingDialog}
+                                      orderDetails={product}
+                                    />
+                                  </Box>
+                                )}
+                              </Grid>
                             </Grid>
-                          </Grid>
-                          <Divider />
-                        </Box>
-                      ))}
+                            <Divider />
+                          </Box>
+                        )
+                      )}
                       <Divider />
                       <Stack
                         direction="row"
